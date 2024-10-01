e224468be7033ac2850d9fd75bcc41084c778d1d
Neeraj574-web <117840679+Neeraj574-web@users.noreply.github.com>
2024-10-01 23:14:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import musicLibrary
+import requests
+from openai import OpenAI
+from gtts import gTTS
+import pygame
+import os
+
+# pip install pocketsphinx
+
+recognizer = sr.Recognizer()
+engine = pyttsx3.init() 
+newsapi = "<Your Key Here>"
+
+def speak_old(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def speak(text):
+    tts = gTTS(text)
+    tts.save('temp.mp3') 
